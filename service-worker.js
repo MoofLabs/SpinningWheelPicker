dd9c31b19042587624a7846ce3521f13c6af47d1
Matthew Bonhage <mbonhage@me.com>
2025-11-14 21:02:58 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'spinning-wheel-v1';
+const urlsToCache = [
+  '/SpinningWheelPicker/',
+  '/SpinningWheelPicker/index.html',
+  '/SpinningWheelPicker/icons/icon-192.png',
+  '/SpinningWheelPicker/icons/icon-512.png',
+  '/SpinningWheelPicker/icons/icon-180.png'
+];
+
+// Install service worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update service worker and clean old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
